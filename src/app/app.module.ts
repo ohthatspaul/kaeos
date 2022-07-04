db06caad23f18ccb6c24cdc12e8ab38a9625b9d2
talentmavingire <mavingiretalent@gmail.com>
2022-07-04 23:33:57 +0200

    Add Clarity and BrowserAnimation modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, IssueListComponent],
+  imports: [BrowserModule, ClarityModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
