ee688567bc337ea431350908e898081404086909
talentmavingire <mavingiretalent@gmail.com>
2022-07-05 00:45:20 +0200

    mock issue data

--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issue';
+import { issues } from '../assets/mock-issues';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssuesService {
-  private issues: Issue[] = [];
+  private issues: Issue[] = [...issues];
 
   constructor() {}
 
