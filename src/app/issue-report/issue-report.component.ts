550fe20c3da8063295eeb98fa8fe5aafc0816ada
talentmavingire <mavingiretalent@gmail.com>
2022-07-05 19:29:25 +0200

    add validation for the form data submission

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IssuesService } from '../issues.service';
 
 @Component({
@@ -15,18 +15,25 @@ export class IssueReportComponent implements OnInit {
     private issueService: IssuesService
   ) {}
 
-  addIssue() {
-    // pass the value of each form control using the value property of the issueForm object.
+  @Output() formClose = new EventEmitter();
 
+  addIssue() {
+    //mark all forms as touched so user can see validation message
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+    // pass the value of each form control using the value property of the issueForm object.
     this.issueService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
   }
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title: [''],
+      title: ['', Validators.required],
       description: [''],
-      priority: [''],
-      type: [''],
+      priority: ['', Validators.required],
+      type: ['', Validators.required],
     });
   }
 }
