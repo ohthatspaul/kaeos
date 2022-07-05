d62eb54529b85fa17507cca62519164550418029
talentmavingire <mavingiretalent@gmail.com>
2022-07-05 11:53:45 +0200

    import BrowserAnimationModule and ReactiveFormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
-  declarations: [AppComponent, IssueListComponent],
-  imports: [BrowserModule, ClarityModule, BrowserAnimationsModule],
+  declarations: [AppComponent, IssueListComponent, IssueReportComponent],
+  imports: [
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
